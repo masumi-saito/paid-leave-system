558b89e0257585accc0831207c94a20db4ad428a
m_saito <m.saito@hitokuse.com>
2020-05-25 17:14:58 +0900

    no message

--- app/controllers/apply_controller.rb
+++ app/controllers/apply_controller.rb
@@ -1,7 +1,11 @@
 class ApplyController < ApplicationController
 
   def create
-    p params[:]
+    @apply = Apply.new(apply_params)
   end
 
+  private
+  def apply_params
+    params.require(:apply).permit(:start_date, :end_date, :type, :remarks)
+  end
 end
